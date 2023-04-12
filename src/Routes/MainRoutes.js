fe6290d55e8056d0772d1709f237c0d614397ea7
ldv0312 <mnn1994kg@gmail.com>
2023-04-12 19:48:25 +0600

    Contacts created

--- src/Routes/MainRoutes.js
+++ src/Routes/MainRoutes.js
@@ -1,9 +1,13 @@
 import React from "react";
 import HomePage from "../Pages/HomePage";
 import { Route, Routes } from "react-router-dom";
+import Contacts from "../components/Contacts/Contacts";
 
 const MainRoutes = () => {
-  const PUBLIC_ROUTES = [{ link: "/", Element: <HomePage />, id: 1 }];
+  const PUBLIC_ROUTES = [
+    { link: "/", Element: <HomePage />, id: 1 },
+    { link: "/contacts", Element: <Contacts />, id: 2 },
+  ];
   return (
     <>
       <Routes>
